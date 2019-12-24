4b45ede0215d155beca9ff76d0af8c40b6e1ff15
Emma Eliasson <gurgelsnurp@hotmail.se>
2019-12-24 18:27:53 +0100

    Movement done

---
+++ Assets/Scripts/PlayerInteract.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInteract : MonoBehaviour
+{
+    public List<GameObject> currentObjList;
+
+    void Update()
+    {
+        if(Input.GetButtonDown("Confirm") && currentObjList.Count > 0)
+        {
+            //open the object's action menu
+            Debug.Log(currentObjList[currentObjList.Count-1].name + " says hi.");
+            //TODO: check on how to make npc's
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("Entered an object.");
+        currentObjList.Add(collision.gameObject);
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        Debug.Log("Exited an object.");
+        currentObjList.Remove(collision.gameObject);
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    Rigidbody2D rb;
+    Vector2 movement;
+    public float speed = 5;
+    //float verticalIncline = 0.4f;
+
+    void Start()
+    {
+        //instead of dragging the component to script inside unity:
+        rb = GetComponent<Rigidbody2D>();
+    }
+    void Update() //input
+    {
+        movement = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+    }
+    void FixedUpdate() //movement
+    {
+        //make vertical movement incline
+        //if (Mathf.Abs(movement.y) > 0) movement.x = (movement.y > 0 ? movement.x + verticalIncline : movement.x - verticalIncline);
+
+        //move player
+        if (Mathf.Abs(movement.x) > 0 && Mathf.Abs(movement.y) > 0)
+            rb.MovePosition((Vector2)transform.position + movement.normalized * speed * Time.fixedDeltaTime);
+        else
+            rb.MovePosition((Vector2)transform.position + movement * speed * Time.fixedDeltaTime);
+    }
+}
